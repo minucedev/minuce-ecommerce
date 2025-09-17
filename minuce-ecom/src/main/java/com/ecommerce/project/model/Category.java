a117a444dd383d4d175535d618826fd2b9a3942b
minucedev <ngnmihpuc.1502@gmail.com>
2025-09-17 09:59:03 +0700

    DI and IoC hand on learning

---
+++ minuce-ecom/src/main/java/com/ecommerce/project/model/Category.java
@@ -0,0 +1,22 @@
+package com.ecommerce.project.model;
+
+public class Category {
+    private Long categoryID;
+    private String categoryName;
+
+    public Long getCategoryID() {
+        return categoryID;
+    }
+
+    public void setCategoryID(Long categoryID) {
+        this.categoryID = categoryID;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+}
