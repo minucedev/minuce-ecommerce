2ad4eefafc2b86452409e87da1f77b528c4142ac
minucedev <ngnmihpuc.1502@gmail.com>
2025-09-17 09:09:45 +0700

    spring boot project starter

---
+++ minuce-ecom/src/main/java/com/ecommerce/project/MinuceEcomApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinuceEcomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinuceEcomApplication.class, args);
+	}
+
+}
